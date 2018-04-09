e0281c0568745fb3d96144936828949f61d9cb68
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-09 04:16:08 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,7 +9,7 @@ class LandmarksController < ApplicationController
     erb :"/landmarks/new"
   end
 
-  post '/figures' do
+  post '/landmarks' do
     @figure = Figure.new(params[:figure])
     @figure.titles.build(params[:title]) if !params[:title][:name].empty?
     @figure.landmarks.build(params[:landmark]) if !params[:landmark][:name].empty?
