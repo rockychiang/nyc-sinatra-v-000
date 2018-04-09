68472b2ac66a92e411b290a9d7ffc83a1d9112a4
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-09 04:46:55 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,39 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+
+  get '/figures/new' do
+    erb :"/figures/new"
+  end
+
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    @figure.titles.build(params[:title]) if !params[:title][:name].empty?
+    @figure.landmarks.build(params[:landmark]) if !params[:landmark][:name].empty?
+    @figure.save
+    redirect "/figures"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :"/figures/show"
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :"/figures/edit"
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    @figure.titles.build(params[:title]) if !params[:title][:name].empty?
+    @figure.landmarks.build(params[:landmark]) if !params[:landmark][:name].empty?
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
+end
