00e2fb02e366ffa4668520c013d09c3a4b4bf1c7
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-09 04:17:33 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -18,22 +18,22 @@ class LandmarksController < ApplicationController
   end
 
   get '/figures/:id' do
-    @figure = Figure.find(params[:id])
+    @landmark = Figure.find(params[:id])
     erb :"/figures/show"
   end
 
   get '/figures/:id/edit' do
-    @figure = Figure.find(params[:id])
+    @landmark = Figure.find(params[:id])
     erb :"/figures/edit"
   end
 
   post '/figures/:id' do
-    @figure = Figure.find(params[:id])
-    @figure.update(params[:figure])
-    @figure.titles.build(params[:title]) if !params[:title][:name].empty?
-    @figure.landmarks.build(params[:landmark]) if !params[:landmark][:name].empty?
-    @figure.save
-    redirect "/figures/#{@figure.id}"
+    @landmark = Figure.find(params[:id])
+    @landmark.update(params[:figure])
+    @landmark.titles.build(params[:title]) if !params[:title][:name].empty?
+    @landmark.landmarks.build(params[:landmark]) if !params[:landmark][:name].empty?
+    @landmark.save
+    redirect "/figures/#{@landmark.id}"
   end
 
 end
